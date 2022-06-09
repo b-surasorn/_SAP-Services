4af0a27442be5ccfe51e899ad8129253915253dc
boswachii <boss.surasorn@gmail.com>
2022-06-09 23:01:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import urllib
 def select_and_insert_to_table():
 
     # connect to staking server and transform the data to dataframe using read_sql_query
-    con_string = pyodbc.connect("driver=ODBC Driver 17 for SQL Server;server=SBNDCDSSTGD.thaibev.com;database=SAP_DATA_Staging;uid=pysapdsusr;pwd=Pys@pdK2xT3Eu$S5;")
+    con_string = pyodbc.connect("driver=ODBC Driver 17 for SQL Server;server=SBNDCDSSTGD.thaibev.com;database=SAP_DATA_Staging;uid=pysapdsusr;pwd=;")
     df = pd.read_sql_query('SELECT KUNNR FROM KNVV_B1P',con_string)
     df.rename(columns={
         'KUNNR':'CUSTOMER_MASTER'
     },inplace=True)
     
     # insert dataframe to data mart server using to_sql
-    quoted = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};SERVER=SBNDCDSWHDEV.thaibev.com;DATABASE=DATA_WH_DEV;uid=pysapdsusr;pwd=Pys@pdK2xT3Eu$S5;")
+    quoted = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};SERVER=SBNDCDSWHDEV.thaibev.com;DATABASE=DATA_WH_DEV;uid=pysapdsusr;pwd=;")
     engine = create_engine('mssql+pyodbc:///?odbc_connect={}'.format(quoted))
     df.to_sql('O_OUTPUT_SALES_VIEW_B1P', schema='dbo', con = engine, if_exists='replace', index=False)
     
